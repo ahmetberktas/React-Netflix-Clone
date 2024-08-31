a4ace7b664da99395e3ec300267aa27a12efc657
ahmetberktas <ahmetberktas50@gmail.com>
2024-08-31 23:06:10 +0300

    dosya yapısı olusturuldu

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # React Netflix Clone
+
+# Kaynaklar
+
+- react-router-dom
+- axios
+- bootstrap
+- @splidejs/react-splide
+- redux
+- react-redux
+- redux-thunk
+
+# API
+
+- API: https://developer.themoviedb.org/reference/intro/getting-started
--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MainPage from "./pages/MainPage";
+import DetailPage from "./pages/DetailPage";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Header></Header>
+      <Routes>
+        <Route path="/" element={<MainPage></MainPage>}></Route>
+        <Route path="/detail" element={<DetailPage></DetailPage>}></Route>
+      </Routes>
+      <Footer></Footer>
+    </BrowserRouter>
+  );
 };
 
 export default App;
